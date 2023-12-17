a1f9caf7631d740064f27526e68689735479c085
charliejp21 <charliejp21@gmail.com>
2023-12-16 19:17:30 -0600

    Iniciando proyecto con react native

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text
+} from 'react-native';
+const App = () => {
+
+  return (
+
+    <SafeAreaView>
+
+      <Text>Hola mundo </Text>
+
+    </SafeAreaView>   
+  )
+}
+
+export default App;
